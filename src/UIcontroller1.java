21d0946bccf211482f65991a5fb1932248d31212
HarleyAppleChoi <appleno56@gmail.com>
2019-04-04 19:23:18 +0100

    Update UIcontroller1.java

--- src/UIcontroller1.java
+++ src/UIcontroller1.java
@@ -432,6 +432,53 @@ public class UIcontroller1 {
 		}
 		return inverted_image;
 	}
+	
+	private Image NearestNeighbor(Image image,int nh,int nw) {
+		// Find the width and height of the image to be process
+		int width = (int) image.getWidth();
+		int height = (int) image.getHeight();
+		// Create a new image of that width and height
+		WritableImage inverted_image = new WritableImage(width, height);
+		// Get an interface to write to that image memory
+		PixelWriter inverted_image_writer = inverted_image.getPixelWriter();
+		// Get an interface to read from the original image passed as the parameter to
+		// the function
+		PixelReader image_reader = image.getPixelReader();
+		
+		Color[][] imageA = new Color[height][width];
+
+			// Iterate over all pixels
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++) {
+					imageA[y][x] = image_reader.getColor(x, y);
+				}
+			}
+		
+		
+		// Iterate over all pixels
+		for (int y = 0; y < nh; y++) {
+			for (int x = 0; x < nw; x++) {
+				
+				int nowY = (int) (y* ((float)height/(float)nh));
+				int nowX = (int) (x* ((float)width/(float)nw));
+				
+				
+				
+				// For each pixel, get the colour
+				Color color = image_reader.getColor(x, y);
+				// Do something (in this case invert) - the getColor function returns colours as
+				// 0..1 doubles (we could multiply by 255 if we want 0-255 colours)
+				color = imageA[nowY][nowX];
+				// Note: for gamma correction you may not need the divide by 255 since getColor
+				// already returns 0-1, nor may you need multiply by 255 since the Color.color
+				// function consumes 0-1 doubles.
+
+				// Apply the new colour
+				inverted_image_writer.setColor(x, y, color);
+			}
+		}
+		return inverted_image;
+	}
 
 	/**
 	 * This method process the image by given power correction.
@@ -1628,6 +1675,10 @@ public class UIcontroller1 {
 		}
 		return inverted_image;
 	}
+	
+	
+	
+	
 
 	private Image nonLinearFilter(Image image) {
 		// Find the width and height of the image to be process
